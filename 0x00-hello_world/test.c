b4feb6adca4bbc45dc4f5f3892168639cfe193c7
SCCSMARTCODE <sccsmart247@gmail.com>
2023-07-13 04:39:23 -0700

    0x00-hello_world

---
+++ 0x00-hello_world/test.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main()
+{
+        int day;
+        char name[50];
+        char month[18];
+
+        printf("your name, month of birth, day of birth:\n");
+        scanf("%s %s %d", name, month, &day);
+
+        printf("ooh %s you are a %s born \nyour birthday is on %dth of %s\n", name,month,day,month);
+
+        return (0);
+}
