ef08e7375c84e9a5f6ea74b46b85762d1259a396
SCCSMARTCODE <sccsmart247@gmail.com>
2023-07-14 09:54:28 -0700

    ./5-print_numbers

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (success)
+ */
+
+int main(void)
+{
+	int luck;
+
+	for(luck = 0; luck <= 9; luck++)
+	{
+		printf("%d", luck);
+	}
+	printf("\n");
+
+	return (0);
+}
