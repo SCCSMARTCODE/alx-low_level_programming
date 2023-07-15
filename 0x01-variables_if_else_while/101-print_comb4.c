6331374ca00f1c8c34c5b17900dd9741e7c51c59
SCCSMARTCODE <sccsmart247@gmail.com>
2023-07-15 08:42:55 -0700

    ./101-print_comb4

---
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -0,0 +1,37 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (success)
+ */
+int main(void)
+{
+	int hundredsDigit;
+
+	for (hundredsDigit = 0; hundredsDigit <= 7; hundredsDigit++)
+	{
+		int tensDigit;
+
+		for (tensDigit = hundredsDigit + 1; tensDigit <= 8; tensDigit++)
+		{
+			int unitsDigit;
+
+			for (unitsDigit = tensDigit + 1; unitsDigit <= 9; unitsDigit++)
+			{
+				putchar(hundredsDigit + '0');
+				putchar(tensDigit + '0');
+				putchar(unitsDigit + '0');
+
+				if (!(hundredsDigit == 7 && tensDigit == 8 && unitsDigit == 9))
+				{
+
+					putchar(',');
+					putchar(' ');
+				}
+			}
+		}
+	}
+
+	return (0);
+}
