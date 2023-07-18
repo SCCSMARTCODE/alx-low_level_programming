4cff9526c4759b967b1d11ea86e4bfa6d43df725
SCCSMARTCODE <sccsmart247@gmail.com>
2023-07-18 12:05:21 -0700

    ghfasd

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * add - check the code
+ *
+ * @x: first value
+ *
+ * @y: second value
+ *
+ * Return: Ans
+ */
+
+int add(int x, int y)
+{
+	int ans;
+
+	ans = x + y;
+
+	return (ans);
+}
