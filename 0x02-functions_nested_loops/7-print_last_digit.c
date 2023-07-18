58f0b48dc821b8ce3e8063831eafcd60c4ef53b9
SCCSMARTCODE <sccsmart247@gmail.com>
2023-07-18 11:37:33 -0700

    ./7-last_digit

---
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * print_last_digit - print the last digit of a number.
+ *
+ * Return: int no
+ *
+ * @no: needed no
+ */
+
+int print_last_digit(int no)
+{
+	int ans;
+
+	ans = no % 10;
+
+	_putchar(ans + '0');
+
+	return (ans);
+}
