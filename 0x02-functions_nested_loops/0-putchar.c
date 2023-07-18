4a5928e3c36e7495cc63f58a3b29f16729ee51ff
SCCSMARTCODE <sccsmart247@gmail.com>
2023-07-18 07:51:23 -0700

    _putchar

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (success)
+ */
+
+int main(void)
+{
+	int i;
+	char putch[10] = "_putchar";
+
+	for (i = 0; i < 8; i++)
+	{
+		putchar(putch[10]);
+		putchar('\n');
+	}
+
+	return (0);
+}
