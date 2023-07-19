20bbe8ac3ea2dc51c71a6924ce463627da3e23e4
SCCSMARTCODE <sccsmart247@gmail.com>
2023-07-19 05:43:59 -0700

    completly done

--- 0x02-functions_nested_loops/102-fibonacci.c
+++ 0x02-functions_nested_loops/102-fibonacci.c
@@ -6,24 +6,26 @@
  * Return: 0 (SUCCESS)
  */
 
-int main() {
+int main(void)
+{
 	int n = 50;
 	long int fib[50];
 	int i;
-    
+
 	fib[0] = 1;
 	fib[1] = 2;
-    
+
 	for (i = 2; i < n; i++)
 	{
-		fib[i] = fib[i-1] + fib[i-2];
+		fib[i] = fib[i - 1] + fib[i - 2];
 	}
 
-	for (i = 0; i < n - 1; i++) {
+	for (i = 0; i < n - 1; i++)
+	{
 		printf("%ld, ", fib[i]);
 	}
 
-	printf("%ld\n", fib[n-1]);
+	printf("%ld\n", fib[n - 1]);
 
-	return 0;
+	return (0);
 }
