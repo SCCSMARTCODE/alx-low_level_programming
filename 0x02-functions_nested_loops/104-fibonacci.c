82e8de70324796e7103e3c6c8cfd3f6939fa0a9e
SCCSMARTCODE <sccsmart247@gmail.com>
2023-07-19 09:44:43 -0700

    hmm

--- 0x02-functions_nested_loops/104-fibonacci.c
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -7,15 +7,15 @@
  */
 int main(void)
 {
-	unsigned long long first_no;
-	unsigned long long sec_no;
-	unsigned long long counting_no;
+	unsigned long first_no;
+	unsigned long sec_no;
+	unsigned long counting_no;
 	int i;
 
 	first_no = 1;
 	sec_no = 2;
 
-	printf("%llu, %llu, ", first_no, sec_no);
+	printf("%lu, %lu, ", first_no, sec_no);
 
 	for (i = 2; i < 98; i++)
 	{
@@ -24,7 +24,7 @@ int main(void)
 		first_no = sec_no;
 		sec_no = counting_no;
 
-		printf("%llu", counting_no);
+		printf("%lu", counting_no);
 
 		if (i != 97)
 		{
