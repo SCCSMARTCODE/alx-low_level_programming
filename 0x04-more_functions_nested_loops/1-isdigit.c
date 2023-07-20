21cfc810498ed05fee90077cbe6d62dfadd0e0d3
SCCSMARTCODE <sccsmart247@gmail.com>
2023-07-20 06:30:42 -0700

    C - More functions, more nested loops

---
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -0,0 +1,19 @@
+/**
+ * _isdigit - Write a function that checks for a digit (0 through 9)
+ *
+ * @c: arguement
+ *
+ * Return: 1 if digit otherwise 0
+ */
+
+int _isdigit(int c)
+{
+	if (c >= 0 && c <= 9)
+	{
+		return (1);
+	}
+	else
+	{
+		return (0);
+	}
+}
