7358538e3207ad97d0e4cb64a1555ef847bf5aa3
SCCSMARTCODE <sccsmart247@gmail.com>
2023-07-20 07:00:57 -0700

    C - More functions, more nested loops

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * print_numbers -  prints the numbers, from 0 to 9
+ *
+ * Return: void
+ */
+
+void print_numbers(void)
+{
+	int no;
+
+	for (no = 0; no <= 9; no++)
+	{
+		_putchar(no + '0');
+	}
+}
