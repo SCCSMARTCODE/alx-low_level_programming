69374f47aa387e9f8fd13987830a51ad1c6e35a1
SCCSMARTCODE <sccsmart247@gmail.com>
2023-07-20 06:49:39 -0700

    C - More functions, more nested loops

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,19 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * mul - Write a function that multiplies two integers
+ *
+ * @a: arguement 1
+ *
+ * @b: arguement 2
+ *
+ * Return: 1 if digit otherwise 0
+ */
+
+int mul(int a, int b)
+{
+	int ans = a * b;
+
+	return (ans);
+}
