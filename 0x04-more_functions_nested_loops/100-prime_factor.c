6d9462eddff8805367df1ba836f551f7eafca5a2
SCCSMARTCODE <sccsmart247@gmail.com>
2023-07-20 17:23:34 -0700

    gcc -Wall -pedantic -Werror -Wextra -std=gnu89 100-prime_factor.c -o 100-prime_factor -lm     gives me tough time

--- 0x04-more_functions_nested_loops/100-prime_factor.c
+++ 0x04-more_functions_nested_loops/100-prime_factor.c
@@ -1,23 +1,32 @@
 #include <stdio.h>
 
+/**
+ * main - Finds and prints the largest prime factor of a given number.
+ *
+ * Return: Always 0.
+ */
+
 int main(void)
 {
-	long number = 612852475143;
-	long factor = 2;
-	long largest_prime_factor = 0;
+long number = 612852475143;
+long factor = 2;
+long largest_prime_factor = 0;
 
-	while (number > 1)
-	{
-	if (number % factor == 0)
-	largest_prime_factor = factor;
-	while (number % factor == 0)
-	{
-	number /= factor;
-	}
-	}
+while (number > 1)
+{
+if (number % factor == 0)
+{
+largest_prime_factor = factor;
+while (number % factor == 0)
+{
+number /= factor;
+}
+}
+factor++;
+}
 
-	printf("%ld\n", largest_prime_factor);
+printf("%ld\n", largest_prime_factor);
 
-	return (0);
+return (0);
 }
 
