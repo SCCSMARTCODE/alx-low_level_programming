feeee23e60d520d37a13913c6238077b9359c6ee
SCCSMARTCODE <sccsmart247@gmail.com>
2023-07-24 09:58:40 -0700

    we are getting there

---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,15 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * _puts -  function that prints a string, followed by a new line
+ *
+ * @str: Arguement
+ *
+ * Return: Void
+ */
+
+void _puts(char *str)
+{
+	puts(str);
+}
