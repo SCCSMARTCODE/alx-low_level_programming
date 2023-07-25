00fc71705427cc62fef138eb296d91ae11a6c6a3
SCCSMARTCODE <sccsmart247@gmail.com>
2023-07-24 17:31:29 -0700

    really getting done

---
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -0,0 +1,33 @@
+#include "main.h"
+
+/**
+ * _atoi - Converts a string to an integer
+ *
+ * @s: Pointer to the string to be converted
+ *
+ * Return: The integer converted from the string
+ */
+int _atoi(char *s)
+{
+	int sign = 1;
+	int result = 0;
+
+	while (*s)
+	{
+	if (*s == '-')
+	sign = -sign;
+
+	if (*s >= '0' && *s <= '9')
+	{
+	result = result * 10 + (*s - '0');
+	}
+	else if (result != 0)
+	{
+	break;
+	}
+
+	s++;
+	}
+
+	return (result * sign);
+}
