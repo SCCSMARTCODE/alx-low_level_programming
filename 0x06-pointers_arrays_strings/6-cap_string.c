23898b8ea4320eda5d61877fe7737d6c2c3a5bbb
SCCSMARTCODE <sccsmart247@gmail.com>
2023-07-27 10:34:22 -0700

    project 6

---
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -0,0 +1,35 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * cap_string - capitalizes all words of a string.
+ *
+ * @str: needed
+ *
+ * Return: value
+ */
+
+char *cap_string(char *str)
+{
+	int i;
+
+	for (i = 0; str[i] != '\0'; i++)
+	{
+	if (i == 0)
+	{
+	if (str[i] >= 'a' && str[i] <= 'z')
+	str[i] -= 32;
+	}
+	else if (str[i] == ' ' || str[i] == '\t' || str[i] == '\n' ||
+	str[i] == ',' || str[i] == ';' || str[i] == '.' ||
+	str[i] == '!' || str[i] == '?' || str[i] == '"' ||
+	str[i] == '(' || str[i] == ')' || str[i] == '{' ||
+	str[i] == '}')
+	{
+	if (str[i + 1] >= 'a' && str[i + 1] <= 'z')
+	str[i + 1] -= 32;
+	}
+	}
+	return (str);
+
+}
