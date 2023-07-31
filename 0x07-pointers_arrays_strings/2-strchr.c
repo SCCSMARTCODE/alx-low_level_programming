9939dd3d9272ab516efb3bd37794efd5522cfc31
SCCSMARTCODE <sccsmart247@gmail.com>
2023-07-31 13:45:26 -0700

    rutting

--- 0x07-pointers_arrays_strings/2-strchr.c
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -12,17 +12,15 @@
 
 char *_strchr(char *s, char c)
 {
-int i = 0;
-
-
-	while (s[i] != '\0')
+	while (*s)
 	{
-	if (s[i] == c)
-	{
-	return (s + i);
+	if (*s == c)
+	return s;
+	s++;
 	}
-	i++;
-	}
+
+	if (*s == c)
+	return (s);
 
 return (NULL);
 }
