a8ffa5e9a9be2f36892fdebe1707405f014a2141
SCCSMARTCODE <sccsmart247@gmail.com>
2023-07-31 12:58:16 -0700

    moving through code

---
+++ 0x07-pointers_arrays_strings/7-print_chessboard.c
@@ -0,0 +1,21 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * print_chessboard - prints the chessboard.
+ * @board: 2D array of characters representing the chessboard
+ */
+void print_chessboard(char (*a)[8])
+{
+	int c, b;
+
+	for (c = 0; c < 8; c++)
+	{
+		for (b = 0; b < 8; b++)
+		{
+			_putchar (a[c][b]);
+		}
+		_putchar('\n');
+	}
+
+}
