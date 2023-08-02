c7426758152d70f67acac07130f5440cf8f751ea
SCCSMARTCODE <sccsmart247@gmail.com>
2023-08-02 13:15:43 -0700

    doing hard things

---
+++ 0x08-recursion/2-strlen_recursion.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * _strlen_recursion - returns the length of a string using recursion.
+ * @s: The input string.
+ *
+ * Return: The length of the string.
+ */
+
+ int _strlen_recursion(char *s)
+{
+	int n = 0;
+
+	if (*s == '\0')
+	{
+	return (n);
+	}
+	n = 1 + _strlen_recursion(s + 1);
+	return (n);
+}
