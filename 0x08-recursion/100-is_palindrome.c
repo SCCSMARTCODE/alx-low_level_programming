bc1ccf16e7561c4bec93abc6e2fe2a014c9f5756
SCCSMARTCODE <sccsmart247@gmail.com>
2023-08-02 19:04:59 -0700

    removing loop from code

--- 0x08-recursion/100-is_palindrome.c
+++ 0x08-recursion/100-is_palindrome.c
@@ -27,15 +27,15 @@ int is_palindrome_helper(char *s, int start, int end)
 {
 	if (start >= end)
 	{
-	return 1;
+	return (1);
 	}
 
 	if (s[start] != s[end])
 	{
-	return 0;
+	return (0);
 	}
 
-	return is_palindrome_helper(s, start + 1, end - 1);
+	return (is_palindrome_helper(s, start + 1, end - 1));
 }
 
 /**
@@ -47,6 +47,6 @@ int is_palindrome(char *s)
 {
 	int len = _strlen(s);
 
-	return is_palindrome_helper(s, 0, len - 1);
+	return (is_palindrome_helper(s, 0, len - 1));
 }
 
