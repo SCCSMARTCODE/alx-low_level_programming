5e1caf4aff3627c05e9611cdab11d13b5a8dbec7
SCCSMARTCODE <sccsmart247@gmail.com>
2023-08-05 14:44:33 -0700

    moving it

--- 0x0A-argc_argv/4-add.c
+++ 0x0A-argc_argv/4-add.c
@@ -1,41 +1,84 @@
 #include <stdio.h>
-#include <stdlib.h>
-#include <ctype.h>
-#include <string.h>
-
-/**
- * main - Write a program that adds positive numbers.
- *
- * @argc: 1
- * @argv: 2
- *
- * Return: 0
- */
-
-int main(int argc, char *argv[])
-	int sum = 0, i;
-
-	if (argc > 1)
-{
-	for (i = 1; i < argc; i++)
+	#include <stdlib.h>
+	#include <ctype.h>
+	#include <string.h>
+	/**
+	 * check_num - check - string there are digit
+	 * @str: array str
+	 *
+	 * Return: Always 0 (Success)
+	 */
+	int check_num(char *str)
 	{
-		int b;
-		char *str;
+		/*Declaring variables*/
+		unsigned int count;
 
-		str = argv[i];
-		for (b = 0; str[b] != '\0'; b++)
+
+		count = 0;
+		while (count < strlen(str)) /*count string*/
+
+
 		{
-			if (str[b] < 48 || str[b] > 57)
+			if (!isdigit(str[count])) /*check if str there are digit*/
+			{
+				return (0);
+			}
+
+
+			count++;
+		}
+		return (1);
+	}
+
+
+	/**
+	 * main - Print the name of the program
+	 * @argc: Count arguments
+	 * @argv: Arguments
+	 *
+	 * Return: Always 0 (Success)
+	 */
+
+
+	int main(int argc, char *argv[])
+
+
+	{
+
+
+		/*Declaring variables*/
+		int count;
+		int str_to_int;
+		int sum = 0;
+
+
+		count = 1;
+		while (count < argc) /*Goes through the whole array*/
+		{
+			if (check_num(argv[count]))
+
+
+			{
+				str_to_int = atoi(argv[count]); /*ATOI --> convert string to int*/
+				sum += str_to_int;
+			}
+
+
+			/*Condition if one of the number contains symbols that are not digits*/
+			else
 			{
 				printf("Error\n");
 				return (1);
 			}
+
+
+			count++;
 		}
+
+
+		printf("%d\n", sum); /*print sum*/
+
+
+		return (0);
 	}
-}
-for (i = 1; i < argc; i++)
-{
-	sum += atoi(argv[i]);
-}
-printf("%d\n", sum);
-return (0);
+
