74d1aa243269f445d1f0b6858089e74ee124f4de
SCCSMARTCODE <sccsmart247@gmail.com>
2023-08-10 05:20:59 -0700

    fix bug

--- 0x0C-more_malloc_free/0-malloc_checked.c
+++ 0x0C-more_malloc_free/0-malloc_checked.c
@@ -1,7 +1,12 @@
 #include "main.h"
 #include <stdlib.h>
-#include <errno.h>
-#include <stdio.h>
+
+/**
+ * *malloc_checked - allocates memory using malloc
+ * @b: number of bytes to allocate
+ *
+ * Return: a pointer to the allocated memory
+ */
 
 void *malloc_checked(unsigned int b)
 {
@@ -9,7 +14,6 @@ void *malloc_checked(unsigned int b)
 
 	if (ptr == NULL)
 	{
-	fprintf(stderr, "malloc failed\n");
 	exit(98);
 	}
 
