eccc905c574f201722e9343a09d0a352fb77adfc
Emmanuel <sccsmart247@gmail.com>
2023-08-24 15:16:42 -0700

    advancing

--- 0x06-pointers_arrays_strings/100-rot13.c
+++ 0x06-pointers_arrays_strings/100-rot13.c
@@ -3,29 +3,32 @@
 
 /**
  * rot13 - applies ROT13 substitution cipher to a string.
- * @str: input string
+ * @val: input string
  *
  * Return: Pointer to the resulting string
  */
 
-
-char *rot13(char *str)
+char *rot13(char *val)
 {
-	int i = 0;
+int i;
 
-	while (str[i] != '\0')
-	{
-	if ((str[i] >= 'a' && str[i] <= 'm') || (str[i] >= 'A' && str[i] <= 'M'))
-	{
-	str[i] += 13;
-	}
-	else if ((str[i] >= 'n' && str[i] <= 'z') || (str[i] >= 'N' && str[i] <= 'Z'))
-	{
-	str[i] -= 13;
-	}
+i = 0;
 
-	i++;
-	}
+while (val[i] != '\0')
 
-	return (str);
+{
+if (((val[i] >= 97) && (val[i] <= 109)) || ((val[i] >= 65) && (val[i] <= 77)))
+{
+val[i] = val[i] + 13;
 }
+else if ((val[i] >= 110 && val[i] <= 122) || (val[i] >= 78 && val[i] <= 90))
+{
+val[i] = val[i] - 13;
+}
+
+i++;
+}
+
+return (val);
+}
+
