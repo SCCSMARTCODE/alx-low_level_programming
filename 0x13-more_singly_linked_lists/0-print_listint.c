f49a825876f32dc7f697ab5ad4c7c7196f5dcd3f
Emmanuel <sccsmart247@gmail.com>
2023-08-28 23:33:28 -0700

    complied

--- 0x13-more_singly_linked_lists/0-print_listint.c
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -9,15 +9,15 @@
  */
 size_t print_listint(const listint_t *h)
 {
-	size_t count = 0;
+	size_t cout = 0;
 
 	while (h)
 	{
 		printf("%d\n", h->n);
 		h = h->next;
-		count++;
+		cout++;
 	}
 
-	return (count);
+	return (cout);
 }
 
