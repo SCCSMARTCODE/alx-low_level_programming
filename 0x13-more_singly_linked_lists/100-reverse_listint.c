e76b97edb77b9bc10d2185323b601e91255f8473
Emmanuel <sccsmart247@gmail.com>
2023-08-29 00:20:23 -0700

    betty

--- 0x13-more_singly_linked_lists/100-reverse_listint.c
+++ 0x13-more_singly_linked_lists/100-reverse_listint.c
@@ -1,5 +1,11 @@
 #include "lists.h"
 
+/**
+ * reverse_listint - Reverses a linked list.
+ * @head: A pointer to the pointer to the first node in the list.
+ *
+ * Return: A pointer to the first node in the new list.
+ */
 listint_t *reverse_listint(listint_t **head)
 {
 	listint_t *prev_node = NULL;
