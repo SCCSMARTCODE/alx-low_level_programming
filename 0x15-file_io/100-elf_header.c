f0ba54d1e58f56ed9e4bbc604aefd0e7043512d8
Emmanuel <sccsmart247@gmail.com>
2023-09-04 12:47:46 -0700

    betty

--- 0x15-file_io/100-elf_header.c
+++ 0x15-file_io/100-elf_header.c
@@ -15,12 +15,12 @@ void print_elf_header(Elf64_Ehdr *header)
 	printf("ELF Header:\n");
 	printf("\tMagic:\t");
 	for (i = 0; i < EI_NIDENT; i++)
-	{
-		printf("%02x ", header->e_ident[i]);
-	}
+	{	printf("%02x ", header->e_ident[i]);	}
 	printf("\n");
-	printf("\tClass:\t\t\t\t%s\n", (header->e_ident[EI_CLASS] == ELFCLASS64) ? "ELF64" : "ELF32");
-	printf("\tData:\t\t\t\t%s\n", (header->e_ident[EI_DATA] == ELFDATA2LSB) ? "2's complement, little endian" : "unknown");
+	printf("\tClass:\t\t\t\t%s\n",
+		(header->e_ident[EI_CLASS] == ELFCLASS64) ? "ELF64" : "ELF32");
+	printf("\tData:\t\t\t\t%s\n", (header->e_ident[EI_DATA] == ELFDATA2LSB) ?
+		"2's complement, little endian" : "unknown");
 	printf("\tVersion:\t\t\t%d (current)\n", header->e_ident[EI_VERSION]);
 	printf("\tOS/ABI:\t\t\t");
 	switch (header->e_ident[EI_OSABI])
@@ -49,8 +49,7 @@ void print_elf_header(Elf64_Ehdr *header)
 			break;
 		default:
 			printf("<unknown: %04x>\n", header->e_type);
-	}
-	printf("\tEntry point address:\t\t0x%lx\n", header->e_entry);
+	}	printf("\tEntry point address:\t\t0x%lx\n", header->e_entry);
 }
 
 /**
@@ -62,39 +61,36 @@ void print_elf_header(Elf64_Ehdr *header)
 int main(int argc, char *argv[])
 {
 	int fd;
+
 	Elf64_Ehdr header;
 
 	if (argc != 2)
 	{
 		fprintf(stderr, "Usage: %s elf_filename\n", argv[0]);
-		return 98;
+		return (98);
 	}
-
 	fd = open(argv[1], O_RDONLY);
 	if (fd == -1)
 	{
 		perror("open");
-		return 98;
+		return (98);
 	}
-
 	if (read(fd, &header, sizeof(header)) != sizeof(header))
 	{
 		perror("read");
 		close(fd);
-		return 98;
+		return (98);
 	}
-
-	if (header.e_ident[EI_MAG0] != ELFMAG0 || header.e_ident[EI_MAG1] != ELFMAG1 ||
+	if (header.e_ident[EI_MAG0] != ELFMAG0 ||
+		header.e_ident[EI_MAG1] != ELFMAG1 ||
 		header.e_ident[EI_MAG2] != ELFMAG2 || header.e_ident[EI_MAG3] != ELFMAG3)
 	{
 		fprintf(stderr, "Not an ELF file.\n");
 		close(fd);
-		return 98;
+		return (98);
 	}
-
 	print_elf_header(&header);
-
 	close(fd);
-	return 0;
+	return (0);
 }
 
