71e5786f53eba1ccc7e77e068e2f482b4aea64a5
SCCSMARTCODE <sccsmart247@gmail.com>
2023-10-12 05:13:17 -0700

    get_dnodeint

--- 0x17-doubly_linked_lists/lists.h
+++ 0x17-doubly_linked_lists/lists.h
@@ -1,6 +1,7 @@
 #ifndef MAIN_H
 #define MAIN_H
 
+#include <stddef.h>
 /**
  * struct dlistint_s - doubly linked list
  * @n: integer
@@ -26,6 +27,5 @@ dlistint_t *get_dnodeint_at_index(dlistint_t *head, unsigned int index);
 int sum_dlistint(dlistint_t *head);
 dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n);
 int delete_dnodeint_at_index(dlistint_t **head, unsigned int index);
-
 
 #endif
