a2384b13a032e4cab20e14bc7d86b15d8844bf40
SCCSMARTCODE <sccsmart247@gmail.com>
2023-10-12 06:21:27 -0700

    password gen

--- 0x17-doubly_linked_lists/103-keygen.c
+++ 0x17-doubly_linked_lists/103-keygen.c
@@ -12,7 +12,7 @@ int main(int argc, char *argv[])
 {
 	unsigned int i, b;
 	size_t len, add;
-	char *charset = "A-CHRDw87lNS0E9B2TibgpnMVys5XzvtOGJcYLU+4mjW6fxqZeF3Qa1rPhdKIouk";
+	char *c = "A-CHRDw87lNS0E9B2TibgpnMVys5XzvtOGJcYLU+4mjW6fxqZeF3Qa1rPhdKIouk";
 	char key[7] = "      ";
 
 	if (argc != 2)
@@ -21,26 +21,26 @@ int main(int argc, char *argv[])
 		return 1;
 	}
 	len = strlen(argv[1]);
-	key[0] = charset[(len ^ 59) & 63];
+	key[0] = c[(len ^ 59) & 63];
 	for (i = 0, add = 0; i < len; i++)
 		add += argv[1][i];
-	key[1] = charset[(add ^ 79) & 63];
+	key[1] = c[(add ^ 79) & 63];
 	for (i = 0, b = 1; i < len; i++)
 		b *= argv[1][i];
-	key[2] = charset[(b ^ 85) & 63];
+	key[2] = c[(b ^ 85) & 63];
 	for (b = argv[1][0], i = 0; i < len; i++)
 	{
 		if ((char)b <= argv[1][i])
 			b = argv[1][i];
 	}
 	srand(b ^ 14);
-	key[3] = charset[rand() & 63];
+	key[3] = c[rand() & 63];
 	for (b = 0, i = 0; i < len; i++)
 		b += argv[1][i] * argv[1][i];
-	key[4] = charset[(b ^ 239) & 63];
+	key[4] = c[(b ^ 239) & 63];
 	for (b = 0, i = 0; (char)i < argv[1][0]; i++)
 		b = rand();
-	key[5] = charset[(b ^ 229) & 63];
+	key[5] = c[(b ^ 229) & 63];
 	printf("%s\n", key);
 	return 0;
 }
