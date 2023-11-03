eb4b240132ffc65bef39d676f8c4876484b17174
SCCSMARTCODE <sccsmart247@gmail.com>
2023-11-03 04:54:15 -0700

    ######

--- 0x1A-hash_tables/6-hash_table_delete.c
+++ 0x1A-hash_tables/6-hash_table_delete.c
@@ -9,12 +9,15 @@ void hash_table_delete(hash_table_t *ht)
 	if (ht == NULL)
 		return;
 
-	for (unsigned long int i = 0; i < ht->size; i++)
+	unsigned long int i;
+	hash_node_t *current, *temp;
+
+	for (i = 0; i < ht->size; i++)
 	{
-		hash_node_t *current = ht->array[i];
+		current = ht->array[i];
 		while (current != NULL)
 		{
-			hash_node_t *temp = current;
+			temp = current;
 			current = current->next;
 			free(temp->key);
 			free(temp->value);
