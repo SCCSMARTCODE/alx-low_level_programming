627d82b1d5f2e6f0764db2e28be57e8b6a788d5e
SCCSMARTCODE <sccsmart247@gmail.com>
2024-02-05 01:43:19 -0800

    searching algorithms

---
+++ 0x1E-search_algorithms/0-linear.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+/**
+ * linear_search - This is a function that performs a silmple linear search
+ *
+ * @array: array is a pointer to the first element of the array to search in
+ * @size: is the number of elements in array
+ * @value: is the value to search for
+ *
+ * Return: If not present or if array is NULL, your function must return -1
+ */
+
+int linear_search(int *array, size_t size, int value)
+{
+	size_t x;
+
+	if (!array)
+	{
+		return (-1);
+	}
+
+	for (x = 0; x < size; x++)
+	{
+		printf("Value checked array[%ld] = [%d]\n", x, array[x]);
+
+		if (value == array[x])
+		{
+			return (x);
+		}
+	}
+
+	return (-1);
+}
---
+++ 0x1E-search_algorithms/1-binary.c
@@ -0,0 +1,39 @@
+#include <stdio.h>
+
+/**
+ * binary_search - Perform binary search on a sorted array
+ * @array: Pointer to the first element of the array
+ * @size: Number of elements in the array
+ * @value: Value to search for
+ *
+ * Return: Index of the found value, or -1 if not found
+ */
+int binary_search(int *array, size_t size, int value)
+{
+	size_t big = 0, end = size - 1, mid = (end + big) / 2, x;
+
+	do {
+		x = big;
+
+		printf("Searching in array:");
+		while (x <= end)
+		{
+			printf(" %d", array[x]);
+			if (x != end)
+				printf(",");
+			x++;
+		}
+		printf("\n");
+
+		if (value > array[mid])
+			big = mid + 1;
+		else if (value < array[mid])
+			end = mid - 1;
+		else
+			return (mid);
+
+		mid = (end + big) / 2;
+	} while (big <= end);
+
+	return (-1); /* Value not found */
+}
